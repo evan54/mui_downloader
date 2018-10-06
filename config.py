25af0f014a450bc3c7ef48e39cac1963e6dd24dc
evan54 <evan54@gmail.com>
2018-10-06 12:11:23 -0400

    added simple config file

---
+++ config.py
@@ -0,0 +1,8 @@
+MEDIA_PATHS = [
+    'WhatsApp/Media/WhatsApp Images/',
+    'DCIM/Camera/',
+    'viber/media/Viber Videos/',
+    'viber/media/Viber Images/',
+]
+
+FTP_ADDRESS = '10.0.0.138:2121/'
