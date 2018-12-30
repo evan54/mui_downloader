53baecef8a9c3e80dc1b31b9121347a456ae402b
evan54 <evan54@gmail.com>
2018-12-30 15:28:15 -0500

    added messenger path

--- config.py
+++ config.py
@@ -7,9 +7,12 @@ MEDIA_PATHS = [Path(x) for x in [
     'viber/media/Viber Videos/',
     'viber/media/Viber Images/',
     'Pictures/scanner/',
+    'Pictures/Messenger/',
 ]]
 
 PICTURE_DIR = Path(HOME_DIR) / Path('Pictures')
 
 FTP_ADDRESS = '10.0.0.138'
+# FTP_ADDRESS = '192.168.1.26'
+# FTP_ADDRESS = '10.0.79.77'
 FTP_PORT = 2121
